6a5e7a7db1785a3588681a6d721559e69af33413
losandluca <lewisalcazar88@gmail.com>
2019-04-02 21:28:03 -0700

    Updates

---
+++ precache-manifest.7d7dd2321f5a8515d5d0432ebf84ba96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "4914fd31462063576aed",
+    "url": "/clicky-game/static/js/main.f5ef633c.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/clicky-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "4914fd31462063576aed",
+    "url": "/clicky-game/static/css/main.ff79dd86.chunk.css"
+  },
+  {
+    "revision": "1a7938ca33fba0b395058c7af161f631",
+    "url": "/clicky-game/index.html"
+  }
+];
